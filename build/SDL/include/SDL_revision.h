6df8d32469aff37882952899e953460994e35035
hdw0 <hdw0@yandex.ru>
2022-12-29 12:25:11 +0300

    Почистил исходники

--- build/SDL/include/SDL_revision.h
+++
@@ -1,6 +0,0 @@
-#define SDL_REVISION "https://github.com/hdw0/CyberBiology3.git@b33e33ac8d4585563889b1caedf9c80ed4c452a1"
-#define SDL_REVISION_NUMBER 0
-
-#ifndef SDL_REVISION
-#define SDL_REVISION ""
-#endif
